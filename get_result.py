d24644594e3e8e77c807e8da9f076c97dba27bce
yueyue1009 <b07902124@csie.ntu.edu.tw>
2022-04-06 00:43:31 +0800

    update metric

--- get_result.py
+++ get_result.py
@@ -1,4 +1,4 @@
-import sys, json, mysql.connector
+import sys, mysql.connector
 from tabulate import tabulate
 
 if __name__ == '__main__':
@@ -13,8 +13,8 @@ if __name__ == '__main__':
     collection = sys.argv[1]
     date_type  = sys.argv[2]
     cursor = mydb.cursor()
-    cursor.execute("SELECT DATE_FORMAT(date_trunc('{}', load_time), '%Y-%m-%d') AS {}, AVG(floor_price) AS floor_price FROM {} GROUP BY DATE_FORMAT(date_trunc('{}', load_time), '%Y-%m-%d') LIMIT 2;".format(date_type, date_type, collection, date_type))
+    cursor.execute("SELECT DATE_FORMAT(date_trunc('{}', load_time), '%Y-%m-%d') AS {}, AVG(floor_price) AS average_price, STDDEV(floor_price) AS standard_deviation FROM {} GROUP BY DATE_FORMAT(date_trunc('{}', load_time), '%Y-%m-%d') LIMIT 2;".format(date_type, date_type, collection, date_type))
 
     results = cursor.fetchall()
 
-    print(tabulate(results, headers=[date_type, 'floor_price'], tablefmt='psql'))
+    print(tabulate(results, headers=[date_type, 'average_price', 'standard_deviation'], tablefmt='psql'))
